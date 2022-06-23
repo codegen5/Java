e72f1de3ebdafb553daa11905c75a461fa5e2875
codegen5 <97366751+codegen5@users.noreply.github.com>
2022-06-23 15:13:13 +0300

    Add files via upload

--- Euler/src/p005.java
+++ Euler/src/p005.java
@@ -7,24 +7,24 @@
 public class p005 {
 
 	public static void main(String[] args) {
-		int sayi = 2;
+		int number = 2;
 		int y = 0;
 		int i = 1;
 		for (int x = 1; x <= 20; x++) {
 			i = x;
-			while (sayi % i != 0)
+			while (number % i != 0)
 				for (int j = 2; j < i; j++) {
 					while (i % j == 0 && i != j)
 						i /= j;
 
 					y = i;
 
-					sayi = sayi * y;
+					number = number * y;
 					break;
 				}
 
 		}
-		System.out.println(sayi);
+		System.out.println(number);
 	}
 
 }
