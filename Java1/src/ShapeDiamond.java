869a38fb62d5e5710ce621c4ca93c4f2c5ac2635
codegen5 <97366751+codegen5@users.noreply.github.com>
2022-06-26 17:06:07 +0300

    Update ShapeDiamond.java

--- Java1/src/ShapeDiamond.java
+++ Java1/src/ShapeDiamond.java
@@ -4,26 +4,47 @@ public class ShapeDiamond {
 
 	public static void main(String[] args) {
 
-		int row, space;
+		int row, space, x = 1;
 
 		Scanner sc = new Scanner(System.in);
 		System.out.print("Enter the number of rows: ");
 		row = sc.nextInt();
 		sc.close();
 
-		space = row;
+		space = row - 1;
 
+		// for number of rows expanding
 		for (int i = 0; i < row; i++) {
-			
+			// print required amount of spaces before blocks
 			for (int y = 0; y < space; y++) {
 				System.out.print(" ");
 			}
+			// print required amount of asterisk blocks
+			for (int z = 0; z < x; z++) {
+				System.out.print("*");
+			}
+			// print new line after each row
+			System.out.println();
 			space--;
-			
-			System.out.print("*");
-
+			x += 2;
 		}
 
-	}
+		space = 1;
 
+		// for number of rows dwindling
+		for (int j = 1; j < row; j++) {
+			// print required amount of spaces before blocks
+			for (int p = 0; p < space; p++) {
+				System.out.print(" ");
+			}
+			// print required amount of asterisk blocks
+			for (int v = 4; v < x; v++) {
+				System.out.print("*");
+			}
+			// print new line after each row
+			System.out.println();
+			space++;
+			x -= 2;
+		}
+	}
 }
